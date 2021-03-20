e104233e94cf42e71fc88917cf2c2fcbdca1d195
omarmattr <omarabomattr@gmail.com>
2021-03-20 12:12:40 -0700

    add coustm layout in table view

---
+++ viewController&tabBar/MyTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyTableCell.swift
+//  viewController&tabBar
+//
+//  Created by omar mattr on 20/03/2021.
+//
+
+import UIKit
+
+class MyTableCell: UITableViewCell {
+
+
+    @IBOutlet weak var titile: UILabel!
+    @IBOutlet weak var imageCell: UIImageView!
+}
